d0746447f83638a5479d80d2c4ad3c04b67b025c
wangbincheng <359477586@qq.com>
2019-04-19 00:10:15 +0800

    Create keras数字识别.py

---
+++ keras数字识别.py
@@ -0,0 +1,40 @@
+# 使用 LeNet 模型对 Mnist 手写数字进行识别
+import keras
+from keras.datasets import mnist
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+# 数据加载
+(train_x, train_y), (test_x, test_y) = mnist.load_data()
+# 输入数据为 mnist 数据集
+train_x = train_x.reshape(train_x.shape[0], 28, 28, 1)
+test_x = test_x.reshape(test_x.shape[0], 28, 28, 1)
+train_x = train_x / 255
+test_x = test_x / 255
+train_y = keras.utils.to_categorical(train_y, 10)
+test_y = keras.utils.to_categorical(test_y, 10)
+# 创建序贯模型
+model = Sequential()
+# 第一层卷积层：6 个卷积核，大小为 5∗5, relu 激活函数
+model.add(Conv2D(6, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)))
+# 第二层池化层：最大池化
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# 第三层卷积层：16 个卷积核，大小为 5*5，relu 激活函数
+model.add(Conv2D(16, kernel_size=(5, 5), activation='relu'))
+# 第二层池化层：最大池化
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# 将参数进行扁平化，在 LeNet5 中称之为卷积层，实际上这一层是一维向量，和全连接层一样
+model.add(Flatten())
+model.add(Dense(120, activation='relu'))
+# 全连接层，输出节点个数为 84 个
+model.add(Dense(84, activation='relu'))
+# 输出层 用 softmax 激活函数计算分类概率
+model.add(Dense(10, activation='softmax'))
+# 设置损失函数和优化器配置
+model.compile(loss=keras.metrics.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+# 传入训练数据进行训练
+model.fit(train_x, train_y, batch_size=128, epochs=2, verbose=1, validation_data=(test_x, test_y))
+# 对结果进行评估
+score = model.evaluate(test_x, test_y)
+print('误差:%0.4lf' %score[0])
+print('准确率:', score[1])
